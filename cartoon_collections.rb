edf6454275695b8454164ac2d286c3668baadb9f
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 22:26:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,5 @@ def find_the_cheese(array)# code an argument here
   end
 end
 
-snacks = ["crackers", "gouda", "thyme"]
-find_the_cheese(snacks)
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
