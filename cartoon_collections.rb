7a365e90470c33ea6421cb68c2f573b95fc26ab6
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 22:25:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,12 +17,10 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
   while(i < array.length)
-    puts "start"
     if (cheese_types.include?(array[i]))
       puts array[i]
     end
     i +=1 
-    puts array[i]
   end
 end
 
